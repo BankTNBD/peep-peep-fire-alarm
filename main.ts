217a87734afdf697abbe52655593989ced33b3f3
IamBanky <59633923+BankTNBD@users.noreply.github.com>
2024-02-04 21:33:16 +0700

    Microbit Code

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+let sensor = 0
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+})
 basic.forever(function () {
-	
+    sensor = pins.digitalReadPin(DigitalPin.P2)
+    if (sensor == 0) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.showIcon(IconNames.No)
+        music.setVolume(255)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.InBackground)
+    } else {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.showIcon(IconNames.Yes)
+    }
 })
