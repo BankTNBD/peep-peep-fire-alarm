b2f3244a5c78517f4e1fed5ff8cde57cb93a8eef
IamBanky <59633923+BankTNBD@users.noreply.github.com>
2024-02-18 20:15:06 +0700

    ✨ : Final

--- main.ts
+++ main.ts
@@ -9,13 +9,16 @@ input.onButtonPressed(Button.B, function () {
 basic.forever(function () {
     smokeSensor = pins.analogReadPin(AnalogPin.P0)
     flameSensor = pins.digitalReadPin(DigitalPin.P8)
-    if (flameSensor == 0 || smokeSensor > 50) {
+    if (flameSensor == 0 || smokeSensor > 800) {
         pins.digitalWritePin(DigitalPin.P12, 1)
         basic.showIcon(IconNames.No)
         music.setVolume(255)
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.InBackground)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.UntilDone)
+        basic.pause(100)
+        control.reset()
     } else {
         pins.digitalWritePin(DigitalPin.P12, 0)
         basic.showIcon(IconNames.Yes)
     }
+    basic.pause(100)
 })
